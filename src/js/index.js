2a5833da2b446106876ada87fc81ea6ce427f277
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-07-02 12:00:18 +0200

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+let itemCount = ul.querySelectorAll('li').length;
+
+button.addEventListener('click', () => {
+  itemCount++;
+  const newItem = document.createElement('li');
+  newItem.textContent = `Item ${itemCount}`;
+  ul.appendChild(newItem);
+});
